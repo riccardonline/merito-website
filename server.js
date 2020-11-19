d39e0b3960070ca0c07fa23a4a3e1b5c82d4b9b7
davidlasry <lasry.david@gmail.com>
2020-11-19 02:30:13 +0200

    fixed dist folder path

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/merito'));
+app.use(express.static('./dist/treo'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/project-name' }
-    );
+    res.sendFile(path.join(__dirname, './dist/treo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
